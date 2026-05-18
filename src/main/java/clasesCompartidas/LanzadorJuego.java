0ae1acea4497fd423fa829d8827590a30ddb63af
isabbb <isabella.escobarva@umariana.edu.co>
2026-05-18 16:46:09 -0300

    configuraciones iniciales del juego

---
+++ src/main/java/clasesCompartidas/LanzadorJuego.java
@@ -0,0 +1,131 @@
+package clasesCompartidas;
+
+import pong.MenuConfig;
+import java.awt.*;
+import javax.swing.*;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+
+import java.awt.event.*;
+
+import com.entropyinteractive.*;
+import pong.Pong;
+
+
+public class LanzadorJuego extends JFrame implements ActionListener {
+    private JGame juego;
+    private Thread t;
+    private final DefaultListModel<String> listaJuegos;
+    private final JList<String> juegos;
+    private final JButton botonConfig;
+    private final JButton botonIniciar;
+    private final JPanel panelImg;
+    private final JScrollPane scrollPane;
+
+    public LanzadorJuego() {
+        setTitle("Sistema de videojuego");
+        setSize(800, 600);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new BorderLayout());
+        setLocationRelativeTo(null);
+
+        // Lista de juegos
+        listaJuegos = new DefaultListModel<>();
+        listaJuegos.addElement("Pong");
+        listaJuegos.addElement("Lemmings");
+        listaJuegos.addElement("Counter-Strike");
+        // Bottones
+        botonConfig = new JButton("Configuración");
+        botonConfig.addActionListener(this);
+        botonIniciar = new JButton("Iniciar juego");
+        botonIniciar.addActionListener(this);
+
+        // Añado los juegos a la barra de juegos
+        juegos = new JList<>(listaJuegos);
+        scrollPane = new JScrollPane(juegos);
+        add(scrollPane, BorderLayout.WEST);
+
+        // Creo el panel de la imagen
+        panelImg = new JPanel();
+        //Cargo las imagenes segun aprete los juegos de la lista 
+        juegos.addListSelectionListener(new ListSelectionListener() {
+            @Override
+            public void valueChanged(ListSelectionEvent e) {
+                if (!e.getValueIsAdjusting()) {
+                    String selected = juegos.getSelectedValue();
+                    panelImg.removeAll();
+                    panelImg.setLayout(new BorderLayout());
+
+                    if ("Lemmings".equals(selected)) {
+                        panelImg.add(new JPanelImage("/lemmings/portada_lemmings.png"));
+                    } else if ("Pong".equals(selected)) {
+                        panelImg.add(new JPanelImage("/pong/portada_pong.png"));
+                    }
+                    panelImg.revalidate();
+                    panelImg.repaint();
+                }
+            }
+        });
+
+        // Meto el panel al frame
+        add(panelImg, BorderLayout.CENTER);
+
+        // Creo un panel para los botones
+        JPanel panelBotones = new JPanel();
+        panelBotones.add(botonIniciar);
+        panelBotones.add(botonConfig);
+
+        // Unifico y agrego panel al frame
+        add(panelBotones, BorderLayout.SOUTH);
+        setVisible(true);
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == botonIniciar) {
+            // Agarra el valor del juego seleccionado en la barra
+            String juegoSeleccionado = juegos.getSelectedValue();
+            if (juegoSeleccionado == null) {
+                JOptionPane.showMessageDialog(LanzadorJuego.this, "Error, seleccione un juego para jugar");
+            }else if ("Pong".equals(juegoSeleccionado)) {
+                juego = new Pong("Pong", 800, 600);
+                t = new Thread() {
+                    public void run() {
+                        juego.run(1.0 / 60.0);
+                    }
+                };
+                t.start();
+            } else if ("Lemming".equals(juegoSeleccionado)) {
+                //Nuevo (modifique lo de adentro del else if por equals para mas mejor)
+                /*juego = new Lemming("Lemming",800,600);
+                t = new Thread() {
+                    public void run() {
+                        juego.run(1.0 / 60.0);
+                    }
+                };
+                t.start();*/
+            } else if ("Counter-Strike".equals(juegoSeleccionado))
+            {
+                JOptionPane.showMessageDialog(LanzadorJuego.this, "No somos tan buenos, todavia no sabemos hacer este");
+            }
+            //Nuevo
+        }
+        if (e.getSource() == botonConfig) {
+            // Agarra el valor del juego seleccionado en la barra
+            String juegoSeleccionado = juegos.getSelectedValue();
+            if (juegoSeleccionado == null) {
+                JOptionPane.showMessageDialog(LanzadorJuego.this, "Error, seleccione un juego para configurar");
+            } else if (juegoSeleccionado.equals("Lemmings") ) {
+                //new MenuConfigLem();
+            } else if (juegoSeleccionado.equals("Pong") ) {
+                new MenuConfig();
+            } else if (juegoSeleccionado.equals("Counter-Strike")) {
+                JOptionPane.showMessageDialog(LanzadorJuego.this, "Configuración en proceso... (Jugate otro juego)");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        new LanzadorJuego();
+    }
+
+}
---
+++ src/main/java/clasesCompartidas/ObjetoGrafico.java
@@ -0,0 +1,55 @@
+package clasesCompartidas;
+import java.awt.*;
+import java.awt.image.*;
+import javax.imageio.*;
+import java.io.*;
+
+
+public abstract class ObjetoGrafico implements ObjetoMovible{
+    protected BufferedImage imagen = null;
+
+    protected double posicionX = 0;
+    protected  double posicionY = 0;
+
+    public ObjetoGrafico(){}
+    public ObjetoGrafico(String filename) {
+        try {
+            imagen = ImageIO.read(getClass().getClassLoader().getResourceAsStream(filename));
+
+        } catch (IOException e) {
+            System.out.println("Excepción en ObjectoGrafico "+e);
+        }
+    }
+
+    public int getAncho(){
+        return imagen.getWidth();
+    }
+    public int getAlto(){return imagen.getHeight();}
+
+    public void setPosicion(double x,double y){
+        this.posicionX = x;
+        this.posicionY = y;
+    }
+
+    public void mostrar(Graphics2D g2) {
+        g2.drawImage(imagen,(int) this.posicionX,(int) this.posicionY,null);
+    }
+
+    public double getX(){
+        return posicionX;
+    }
+
+    public double getY(){
+        return posicionY;
+    }
+    public void setImagen(BufferedImage imagen) {
+    this.imagen = imagen;
+    }
+
+    public BufferedImage getImagen() {
+        return this.imagen;
+    }
+
+    public void setY(double y) {this.posicionY = y;}
+    public void setX(double x){this.posicionX = x;}
+}
