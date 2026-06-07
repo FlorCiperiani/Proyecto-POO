85f6424c6ad4e62a090e11227648eda459daf92f
FlorCiperiani <ciperianiflor@gmail.com>
2026-06-07 14:19:17 -0300

    Modificacion para sprite en pulpo

--- src/main/java/spaceinvaders/Pulpo.java
+++ src/main/java/spaceinvaders/Pulpo.java
@@ -4,4 +4,7 @@ public class Pulpo extends Enemigo {
     public Pulpo(double x, double y, double velocidadInicial) {
         super(x, y, "/AssetsSpace/red.png", 30, velocidadInicial);
     }
+    public Pulpo(double x, double y, String rutaImagen, double velocidadInicial) {
+        super(x, y, rutaImagen, 30, velocidadInicial);
+    }
 }
